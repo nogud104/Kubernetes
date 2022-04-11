fe23b6b4adfa6d0f14c605c7e320eec63b97bf2b
Чувашов Михаил Владимирович <MVChuvashov@sberbank.ru>
2022-04-11 14:09:49 +0300

    final work

--- task_2/count.rb
+++ task_2/count.rb
@@ -1,8 +1,9 @@
 counts = Hash.new(0)
 
 1000.times do
-  output = `curl -s -H "Host: ingress-nginx.test" http://192.168.49.2 | grep 'pod namespace'`
-  counts[output.strip.split.last] += 1
+  # output = `curl -s -H "Host: ingress-nginx.test" http://192.168.49.2 | grep 'pod namespace'`
+  output = `curl -s -H "canary: always" -H "Host: ingress-nginx.test" http://localhost`
+  counts[output] += 1
 end
 
 puts counts
